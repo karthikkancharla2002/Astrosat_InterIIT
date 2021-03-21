884d1b62f25c0c45c412600494a1aac4fa59cb80
Ati <190020009@iitdh.ac.in>
2021-03-21 14:27:52 +0530

    phase 1 gui complete

--- src/display.py
+++ src/display.py
@@ -1,103 +1,40 @@
-import pandas as pd
-from tkinter import *
-from tkinter import ttk
-from matplotlib.figure import Figure 
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk) 
-
-
-file1='D:\Club\Tech sec 2021\Inter IIT 2021 March\ASTROSAT\Astrosat_InterIIT\src\Astrosat_readings_new.csv'
-filedata=pd.read_csv(file1)
-## Storing unique catalog Proposal ids in a set
-catalog_data = filedata[['Proposal_ID']]
-catalog_list = set(catalog_data['Proposal_ID'].array)
-## Applet code
-root = Tk() 
-
-# Wrapper 1 stores the introduction Message:
-w1 = LabelFrame(root)
-# Wrapper 2 stores the remaining Content:
-w2 = LabelFrame(root)
-
-w1.pack(fill="both",expand="yes",padx=10, pady=10)
-w2.pack(fill="both",expand="yes",padx=10, pady=10)
-
-# function to display text when
-# button is clicked
-def showSelectedCatalog(catalog):
-    dfpropid = filedata.loc[filedata['Proposal_ID'] == catalog] 
-    dfpropid = dfpropid.set_index('Serial number')
-    print(dfpropid.head(10))
-    # the figure that will contain the plot 
-    fig = Figure(figsize = (5, 3), 
-                 dpi = 100) 
-    # list of squares 
-    y = [i**2 for i in range(101)] 
-  
-    # adding the subplot 
-    plot1 = fig.add_subplot(111) 
-  
-    # plotting the graph 
-    plot1.plot(y) 
-  
-    # creating the Tkinter canvas 
-    # containing the Matplotlib figure 
-    canvas = FigureCanvasTkAgg(fig, 
-                               master = w1)   
-    canvas.draw() 
-  
-    # placing the canvas on the Tkinter window 
-    canvas.get_tk_widget().pack() 
-  
-    # creating the Matplotlib toolbar 
-    toolbar = NavigationToolbar2Tk(canvas, 
-                                   w1) 
-    toolbar.update() 
-  
-    # placing the toolbar on the Tkinter window 
-    canvas.get_tk_widget().pack()
-
-
-def clicked():
-    canvas_catalog_list = Canvas(w2)
-    canvas_catalog_list.pack(side=LEFT, fill="both", expand="yes")
-
-    yscrollbar = ttk.Scrollbar(w2,orient="vertical", command=canvas_catalog_list.yview )
-    yscrollbar.pack(side=RIGHT, fill= "y")
-
-    canvas_catalog_list.configure(yscrollcommand = yscrollbar.set)
-    canvas_catalog_list.bind('<Configure>', lambda e: canvas_catalog_list.configure(scrollregion = canvas_catalog_list.bbox('all')))
-
-    catalog_list_frame = Frame(canvas_catalog_list)
-    # catalog_list_frame.pack()
-    canvas_catalog_list.create_window((0,0),window = catalog_list_frame, anchor="nw")
-    tableCatagData = []
-    for catag in catalog_list:
-        tableCatagData.append(catag)
-    tableEle = []
-    for catag in tableCatagData:
-        tableEle.append(Button(catalog_list_frame, text = catag, command = lambda: showSelectedCatalog(catag)))
-    for tableLbl in range(len(tableEle)):
-        tableEle[tableLbl].pack()    
-    catView = []
-
-
-
-canvas_intro = Canvas(w1)
-canvas_intro.pack(side=LEFT, fill="both", expand="yes")
-
-
-lbl = Label(canvas_intro, text = "To see the various available catalogs ")
-lbl.grid(row=0, column = 0) 
-btn = Button(canvas_intro, text = "Click here" , fg = "red", command=clicked)
-btn.grid(row = 0, column = 1)
-
-
-# all widgets will be here
-# Execute Tkinter
-root.title("Welcome to ASTROSAT data analyzer")
-root.geometry('500x500')
-
-root.mainloop()
-
-
-
+import numpy as np
+import sys
+import mpld3
+# Set up matplotlib
+import matplotlib.pyplot as plt
+%matplotlib inline
+from astropy.io import ascii
+data = ascii.read("lmxb_hmxb combined_cosmic sources.csv")
+data['RA']
+import astropy.coordinates as coord
+import astropy.units as u
+new = []
+for i in range(len(data)):
+    i_row = data[i] # get the first (ith) row
+    ra = coord.Angle(i_row["RA"], unit=u.hour) # create an Angle object
+#     print(ra.degree) # convert to degrees
+    new.append(ra.degree)  
+ra = coord.Angle(new*u.degree)
+ra = ra.wrap_at(180*u.degree)
+import astropy.coordinates as coord
+import astropy.units as u
+new2 = []
+for i in range(len(data)):
+    i_row = data[i] # get the first (ith) row
+    dec = coord.Angle(i_row["Dec"], unit=u.deg)
+#     print(dec.degree) # convert to degrees
+    new2.append(dec.degree)
+ra = coord.Angle(new*u.degree)
+ra = ra.wrap_at(180*u.degree)
+dec = coord.Angle(new2*u.degree)
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(projection="mollweide")
+t=ax.scatter(ra.radian, dec.radian)
+ax.set_xticklabels(['14h','16h','18h','20h','22h','0h','2h','4h','6h','8h','10h'])
+ax.grid(True)
+N=280
+labels = ['point {0}'.format(i + 1) for i in range(N)]
+tooltip = mpld3.plugins.PointLabelTooltip(t, labels=labels)
+mpld3.plugins.connect(fig, tooltip)
+mpld3.display()
--- src/starmapEd.py
+++ src/starmapEd.py
@@ -1,10 +1,10 @@
 import numpy as np
+
 # Set up matplotlib
 import matplotlib.pyplot as plt
-# %matplotlib inline
-from astropy.io import *
-data = ascii.read("lmxb_hmxb combined_cosmic sources.csv")
-data
+%matplotlib inline
+from astropy.io import ascii
+data = ascii.read("lmxb_hmxb combined_cosmic sources.csv"
 data['RA']
 import astropy.coordinates as coord
 import astropy.units as u
@@ -13,9 +13,7 @@ for i in range(len(data)):
     i_row = data[i] # get the first (ith) row
     ra = coord.Angle(i_row["RA"], unit=u.hour) # create an Angle object
     print(ra.degree) # convert to degrees
-    new.append(ra.degree)
-new   
-len(new)
+    new.append(ra.degree)  
 ra = coord.Angle(new*u.degree)
 ra = ra.wrap_at(180*u.degree)
 import astropy.coordinates as coord
@@ -26,16 +24,15 @@ for i in range(len(data)):
     dec = coord.Angle(i_row["Dec"], unit=u.deg)
     print(dec.degree) # convert to degrees
     new2.append(dec.degree)
-new2   
-len(new2)
 ra = coord.Angle(new*u.degree)
 ra = ra.wrap_at(180*u.degree)
 dec = coord.Angle(new2*u.degree)
 fig = plt.figure(figsize=(8,6))
-ax = fig.add_subplot(111, projection="mollweide")
-ax.scatter(ra.radian, dec.radian)
-fig = plt.figure(figsize=(8,6))
-ax = fig.add_subplot(111, projection="mollweide")
-ax.scatter(ra.radian, dec.radian)
+ax = fig.add_subplot(projection="mollweide")
+t=ax.scatter(ra.radian, dec.radian)
 ax.set_xticklabels(['14h','16h','18h','20h','22h','0h','2h','4h','6h','8h','10h'])
 ax.grid(True)
+labels = ['point {0}'.format(i + 1) for i in range(N)]
+tooltip = mpld3.plugins.PointLabelTooltip(t, labels=labels)
+mpld3.plugins.connect(fig, tooltip)
+mpld3.show()
