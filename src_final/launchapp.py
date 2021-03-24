e7e7deced3327357e6d65d10ae9a4713541df05c
Ati <190020009@iitdh.ac.in>
2021-03-24 21:26:49 +0530

    final push

--- src_final/launchapp.py
+++ src_final/launchapp.py
@@ -8,9 +8,49 @@ import matplotlib.pyplot as plt
 from astropy.io import ascii # main library helps in understanding and processing astronomical  data
 import astropy.coordinates as astro_cords
 import astropy.units as astro_units
+import csv
 
+publications_csv = "./publication.csv"
 sources_csv_file = "lmxb_hmxb combined_cosmic sources.csv"
 astro_csv_file = './Astrosat_readings_new.csv'
+
+Title = 'Title'
+Authors = 'Authors'
+Bibliographi = 'Bibliographi'
+Keywords = 'Keywords'
+Abstract = 'Abstract'
+URL = 'URL'
+
+title_data = []
+auth_data = []
+bibl_data = []
+keyword_data = []
+abs_data = []
+url_data = []
+
+with open(publications_csv, 'r', encoding="utf8") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        if len(row)>0:
+            header = row[0].split()[0][:-1]
+            if header == Title:
+                title_data.append(row[0].split(':')[1].strip())
+            elif header == Authors:
+                auth_data.append(row[0].split(':')[1].strip())
+            elif header == Bibliographi:
+                bibl_data.append(row[0].split(':')[1].strip())
+            elif header == Keywords:
+                keyword_data.append(row[0].split(':')[1].strip())
+            elif header == Abstract:
+                abs_data.append(row[0].split(':')[1].strip())
+            elif header == URL:
+                url_data.append(row[0][5:].strip())
+
+df_publications = pd.DataFrame([title_data, auth_data, bibl_data, keyword_data, abs_data, url_data], index=[Title,Authors,Bibliographi,Keywords,Abstract,URL]).T
+
+def displayPublicationData(source_name, source_name2):
+    df_selected_source_publication = df_publications[(df_publications.Title.str.replace(" ","").str.find(source_name.replace(" ","")) != -1) | (df_publications.Title.str.replace(" ","").str.find(source_name2.replace(" ","")) != -1) ]
+    return df_selected_source_publication
 
 def clickedSourceBox(n_data): 
     clickSourceRoot = Tk()  # creates a new window
@@ -24,6 +64,10 @@ def clickedSourceBox(n_data):
     frame1.pack(fill="both",expand="yes",padx=10, pady=5) # frame 1 is packed such that it fills both axes, is allowed to expand and paddying 10 pixels on x-axis and 5 pixels on y-axis
     file_frame = tk.LabelFrame(wrapper_data, text="Selected Source data") # file_frame is hinged on wrapper_data with text 'Selected Source data'
     file_frame.pack(fill="both",expand="yes",padx=10, pady=5) # file_frame is packed such that it fills both axes, is allowed to expand, and padding 10 pixels x-axis and 5 pixels on y-axis 
+        
+    publications_frame = tk.LabelFrame(wrapper_data, text="Publications")
+    publications_frame.pack(fill="both",expand="yes",padx=10, pady=5)
+
     tv1s = ttk.Treeview(frame1, columns=["Sl.no", "Source_name"], show="headings", height="6") # Treeview shows the tables, is hinged to frame1, gives column ids, headings and height of each cell as 6
     tv1s.heading("Sl.no", text = "Sl.no") #column of ids and provided texts (identifiers)
     tv1s.heading("Source_name", text = "Source Name")  # columns are given names
@@ -33,31 +77,56 @@ def clickedSourceBox(n_data):
     tv1s.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set) # assign the scrollbars to the Treeview Widget
     treescrollx.pack(side="bottom", fill="x") # make the scrollbar fill the x axis of the Treeview widget
     treescrolly.pack(side="right", fill="y") # make the scrollbar fill the y axis of the Treeview widget
+
     tv2 = ttk.Treeview(file_frame, show="headings", height="6") # Treeview shows the tables, is hinged to file_frame, gives column ids, headings and height of each cell as 6
     tv2.place(relheight=1, relwidth=1) # set the height and width of the widget to 100% of its container (frame1).
     treescrolly2 = tk.Scrollbar(file_frame, orient="vertical", command=tv2.yview) # command means update the yaxis view of the widget
     treescrollx2 = tk.Scrollbar(file_frame, orient="horizontal", command=tv2.xview) # command means update the xaxis view of the widget
     tv2.configure(xscrollcommand=treescrollx2.set, yscrollcommand=treescrolly2.set) # assign the scrollbars to the Treeview Widget
     treescrollx2.pack(side="bottom", fill="x") # make the scrollbar fill the x axis of the Treeview widget
-    treescrolly2.pack(side="right", fill="y") # make the scrollbar fill the y axis of the Treeview widget
+    treescrolly2.pack(side="right", fill="y") # make the scrollbar fill the y axis of the Treeview widget    
+    
+    tv3 = ttk.Treeview(publications_frame, show="headings", height="6")
+    tv3.place(relheight=1, relwidth=1)
+    treescrolly3 = tk.Scrollbar(publications_frame, orient="vertical", command=tv3.yview) # command means update the yaxis view of the widget
+    treescrollx3 = tk.Scrollbar(publications_frame, orient="horizontal", command=tv3.xview) # command means update the xaxis view of the widget
+    tv3.configure(xscrollcommand=treescrollx3.set, yscrollcommand=treescrolly3.set) # assign the scrollbars to the Treeview Widget
+    treescrollx3.pack(side="bottom", fill="x") # make the scrollbar fill the x axis of the Treeview widget
+    treescrolly3.pack(side="right", fill="y") # make the scrollbar fill the y axis of the Treeview widget
+   
     for i in range(len(n_data)):
         tv1s.insert('', 'end',values=(i+1,n_data[i])) # inserts data in the tv in the end of the row with the values given
+    
+    def clear_window():
+        tv2.delete(*tv2.get_children())
+        tv3.delete(*tv2.get_children())
+    
     def selectedSource(n_data):
-        # print('selected', n_data) 
-        s = tv1s.selection()[0] 
+        clear_window()
+        s = tv1s.selection()[0]
         selected_source_name = tv1s.item(s,"values")[1]
-        # print('selected',selected_catalog_id)
-        df_astrosat_data = getData(selected_source_name) # matches astrosat datafile with current data
-        if (df_astrosat_data[df_astrosat_data.columns[0]].count())==0: # if it comes out to be 0
-            messagebox.showinfo("information","Source not present in astrosat data set")  # the message is shown
-            return # previous function ends here
-        tv2["column"] = list(df_astrosat_data.columns) # if the current data matches with astrosat data
-        tv2["show"] = "headings" # their heading is shown
+        df_astrosat_data = getData(selected_source_name)
+        astrosat_name = selected_source_name
+        if (df_astrosat_data[df_astrosat_data.columns[0]].count())!=0:
+            astrosat_name = df_astrosat_data['Source_name'].values[0]
+        else:
+            return
+        tv2["column"] = list(df_astrosat_data.columns)
+        tv2["show"] = "headings"
         for column in tv2["columns"]:
             tv2.heading(column, text=column) # let the column heading = column name
         df_rows2 = df_astrosat_data.to_numpy().tolist() # turns the dataframe into a list of lists
         for row in df_rows2:
             tv2.insert("", "end", values=row) # inserts each list into the treeview. 
+        
+        df_publications_data = displayPublicationData(astrosat_name, selected_source_name)
+        tv3["column"] = list(df_publications_data.columns)
+        tv3["show"] = "headings"
+        for column in tv3["columns"]:
+            tv3.heading(column, text=column) # let the column heading = column name
+        df_rows2 = df_publications_data.to_numpy().tolist() # turns the dataframe into a list of lists
+        for row in df_rows2:
+            tv3.insert("", "end", values=row) # inserts each list into the treeview. 
     tv1s.bind("<Double-1>", lambda n_data:  selectedSource(n_data)) # lambda funtion seta parameters that need to be passed, define events with custom parameters
     clickSourceRoot.title("Welcome to ASTROSAT data analyzer")
     clickSourceRoot.geometry('800x800')
@@ -154,8 +223,80 @@ w2.pack(fill="both",expand="yes",padx=10, pady=10) # w2 fills both x & y axes, i
 def makestarmap():
     build_map() 
 
-B = tk.Button(w1, text ="GetStarMap", command = makestarmap) # Button is hinged on w1, when clicked on GetStarMap, makesarmap function would be triggered
-B.pack()
+
+heading = tk.Label( w1, text="VISUALIZATION TOOL FOR ASTROSAT OBSERVATIONS", font=("Arial", 15))
+heading.pack()
+
+file_input_frame_alpha = tk.LabelFrame(w1, text="Import Files")
+file_input_frame_alpha.pack(fill="x",expand="yes",padx=10, pady=10)
+
+# astrosat data file
+file_frame_astrosat_data = tk.LabelFrame(file_input_frame_alpha, text="Astrosat Data File")
+file_frame_astrosat_data.pack(side="left",expand="yes",padx=10, pady=10,fill = tk.BOTH)
+label_file_text = "No File Selected"
+varast = tk.StringVar()
+label_file = ttk.Label(file_frame_astrosat_data, textvariable=varast)
+varast.set(label_file_text[-25:])
+label_file.pack(fill="x",expand="yes",padx=10, pady=2)
+button1_astrosat_data = tk.Button(file_frame_astrosat_data, text="Browse A File", command=lambda: File_dialog(label_file_text, varast))
+button1_astrosat_data.pack(fill="x",expand="yes",padx=10, pady=2)
+button2_astrosat_data = tk.Button(file_frame_astrosat_data, text="Load File", command=lambda: Load_excel_data(astro_csv_file, label_file_text))
+button2_astrosat_data.pack(fill="x",expand="yes",padx=10, pady=2)
+
+# Sources Data
+file_frame_sources_data = tk.LabelFrame(file_input_frame_alpha, text="Sources Data File")
+file_frame_sources_data.pack(side="left",expand="yes",padx=10, pady=10, fill = tk.BOTH)
+label_file_sources_text = "No File Selected"
+varsou = tk.StringVar()
+label_file_sources = ttk.Label(file_frame_sources_data, textvariable=varsou)
+varsou.set( label_file_sources_text[-25:])
+label_file_sources.pack(fill="x",expand="yes",padx=10, pady=2)
+button1_sources_data = tk.Button(file_frame_sources_data, text="Browse A File", command=lambda: File_dialog(label_file_sources_text, varsou))
+button1_sources_data.pack(fill="x",expand="yes",padx=10, pady=2)
+button2_sources_data = tk.Button(file_frame_sources_data, text="Load File", command=lambda: Load_excel_data(sources_csv_file, label_file_sources_text))
+button2_sources_data.pack(fill="x",expand="yes",padx=10, pady=2)
+
+# Publications Data
+file_frame_publications_data = tk.LabelFrame(file_input_frame_alpha, text="Publications Data File")
+file_frame_publications_data.pack(side="left",expand="yes",padx=10, pady=10, fill = tk.BOTH)
+label_file_publications_text = "No File Selected"
+varpub = tk.StringVar()
+label_file_publications = ttk.Label(file_frame_publications_data, textvariable=varpub)
+varpub.set(label_file_publications_text[-25:])
+label_file_publications.pack(fill="x",expand="yes",padx=10, pady=2)
+button1_publications_data = tk.Button(file_frame_publications_data, text="Browse A File", command=lambda: File_dialog(label_file_publications_text, varpub))
+button1_publications_data.pack(fill="x",expand="yes",padx=10, pady=2)
+button2_publications_data = tk.Button(file_frame_publications_data, text="Load File", command=lambda: Load_excel_data(publications_csv, label_file_publications_text))
+button2_publications_data.pack(fill="x",expand="yes",padx=10, pady=2)
+
+
+def File_dialog(label_file, var):
+    """This Function will open the file explorer and assign the chosen file path to label_file"""
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select A File",
+                                          filetype=(("xlsx files", "*.xlsx"),("All Files", "*.*")))
+    label_file = filename
+    var.set(label_file[-25:])
+    return None
+
+def Load_excel_data(pathToSet, label_file):
+    """If the file selected is valid this will load the file into the Treeview"""
+    file_path = label_file
+    try:
+        excel_filename = "{}".format(file_path)
+        print(excel_filename)
+        if excel_filename[-4:] == ".csv":
+            pathToSet = excel_filename
+    except ValueError:
+        tk.messagebox.showerror("Information", "The file you have chosen is invalid")
+        return None
+    except FileNotFoundError:
+        tk.messagebox.showerror("Information", "No such file as {}".format(file_path))
+        return None
+
+
+B = tk.Button(w1, text ="Generate Map", command = makestarmap , bg="white")
+B.pack(fill="both",expand="yes",padx=10, pady=10)
 
 ################ Wrapper 2 GUI #####################################
 
